057231a858118d0b15dbbaaeee693f78caabb2ac
MikusR <mikus.rakis@gmail.com>
2024-07-16 13:35:34 +0000

    16 Jul 2024, 16:35

--- app/api.rb
+++ app/api.rb
@@ -1,13 +1,14 @@
 require 'uri'
 require 'net/http'
 require 'json'
+require './models/merchant'
 
 class ApiTest
   def merchants
     uri = URI('https://simpledebit.gocardless.io/merchants')
     response = JSON.parse(Net::HTTP.get(uri))
     merchants = response.map { |merchant| Merchant.new(merchant) }
-    pp merchants[0].class
+    pp merchants[0].uri
     # test.each { |element| pp element }
   end
 end
