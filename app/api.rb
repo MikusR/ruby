b0ae1dd19b0d1acbcbe9384462954265236010b7
MikusR <mikus.rakis@gmail.com>
2024-07-16 14:51:50 +0000

    16 Jul 2024, 17:51

--- app/api.rb
+++ app/api.rb
@@ -7,15 +7,15 @@ class ApiTest
   def merchants
     uri = URI('https://simpledebit.gocardless.io/merchants')
     response = JSON.parse(Net::HTTP.get(uri))
-    pp Merchant.new(response[0]).payable
-    # response.map { |merchant| Merchant.new(merchant) }
+    # pp Merchant.new(response[0]).payable
+    response.map { |merchant| Merchant.new(merchant) }
   end
 
   def write_csv
+    to = merchants
+    pp to[0].payable
   end
 end
 
 app = ApiTest.new
-merchants = app.merchants
-# pp merchants[0].uri
-# pp app.transactions(merchants[0])
+app.write_csv
