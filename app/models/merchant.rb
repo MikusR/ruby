25a7050a7a64d253c07f26a402055dbdaeecdfcb
MikusR <mikus.rakis@gmail.com>
2024-07-16 14:12:35 +0000

    16 Jul 2024, 17:12

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,5 @@
 class Merchant
-  attr_accessor :id, :iban, :discount, :transactions
+  attr_reader :id, :iban, :discount, :transactions, :amount_sum, :fee_sum
 
   def initialize(merchant)
     @id = merchant
@@ -7,13 +7,13 @@ class Merchant
     @iban = response['iban']
     @discount = response['discount']
     @transactions = response['transactions']
-    
-    @transactions.each do ||
-
+    @amount_sum = 0
+    @fee_sum = 0
+    @transactions.each do |transaction|
+      amount_sum += transaction['amount']
+      fee_sum += transaction['fee']
     end
   end
-
-
 
   def uri
     URI("https://simpledebit.gocardless.io/merchants/#{@id}")
